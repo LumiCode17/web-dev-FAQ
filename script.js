79b305c7d497c69c334ed5b735b1f266e4cfc9ac
Olumide Jones <olumideyjones@yahoo.com>
2022-08-30 16:34:23 +0200

    question added

--- script.js
+++ script.js
@@ -1,64 +1,204 @@
 const showBtn = document.getElementById('showBtn');
 
 showBtn.addEventListener('click', () => {
-  const answer = document.getElementById('ans');
+    const answer = document.getElementById('ans');
 
-  if (answer.style.display === 'none') {
-    // 👇️ this SHOWS the answer
-    answer.style.display = 'block';
-  } else {
-    // 👇️ this HIDES the answer
-    answer.style.display = 'none';
-  }
+    if (answer.style.display === 'none') {
+        // 👇️ this SHOWS the answer
+        answer.style.display = 'block';
+    } else {
+        // 👇️ this HIDES the answer
+        answer.style.display = 'none';
+    }
 });
 /********Quest2*********/
 function myFunction() {
     let x = document.getElementById("ansTwo");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
-  }
+}
 
-  /********Quest3**********/
+/********Quest3**********/
 function myQuest3() {
     let x = document.getElementById("ansThree");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
-  }
+}
 
-   /********Quest4*********/
+/********Quest4*********/
 function myQuest4() {
     let x = document.getElementById("ansFour");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
-  }
+}
 
 
-   /********Quest5*********/
+/********Quest5*********/
 function myQuest5() {
     let x = document.getElementById("ansFive");
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
-  }
+}
 
 
-   /********Quest6*********/
-   function myQuest6() {
+/********Quest6*********/
+function myQuest6() {
     let x = document.getElementById("ansSix");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+  /********Quest7*********/
+  function myQuest7() {
+    let x = document.getElementById("ansSeven");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
   }
+
+    /********Quest8*********/
+    function myQuest8() {
+        let x = document.getElementById("ansEight");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+
+        /********Quest9*********/
+   function myQuest9() {
+    let x = document.getElementById("ansNine");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+    /********Quest10*********/
+    function myQuest10() {
+        let x = document.getElementById("ansTen");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+
+        /********Quest11*********/
+   function myQuest11() {
+    let x = document.getElementById("ansEleven");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+    /********Quest12*********/
+    function myQuest12() {
+        let x = document.getElementById("ansTwelve");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+
+        /********Quest13*********/
+   function myQuest13() {
+    let x = document.getElementById("ansThirteen");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+    /********Quest14*********/
+    function myQuest14() {
+        let x = document.getElementById("ansFourteen");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+
+        /********Quest15*********/
+   function myQuest15() {
+    let x = document.getElementById("ansFifteen");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+    /********Quest16*********/
+    function myQuest16() {
+        let x = document.getElementById("ansSixteen");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+
+        /********Quest17*********/
+   function myQuest17() {
+    let x = document.getElementById("ansSeventeen");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+    /********Quest18*********/
+    function myQuest18() {
+        let x = document.getElementById("ansEighteen");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+
+        /********Quest19*********/
+   function myQuest19() {
+    let x = document.getElementById("ansNineteen");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+    /********Quest20*********/
+    function myQuest20() {
+        let x = document.getElementById("ansTwenty");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
