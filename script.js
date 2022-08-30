95eec9e19d7bce0652a2cda4d181c6b68bfb0afa
Olumide Jones <olumideyjones@yahoo.com>
2022-08-30 15:29:07 +0200

    question added

---
+++ script.js
@@ -0,0 +1,42 @@
+const showBtn = document.getElementById('showBtn');
+
+showBtn.addEventListener('click', () => {
+  const answer = document.getElementById('ans');
+
+  if (answer.style.display === 'none') {
+    // 👇️ this SHOWS the answer
+    answer.style.display = 'block';
+  } else {
+    // 👇️ this HIDES the answer
+    answer.style.display = 'none';
+  }
+});
+/********Quest2*********/
+function myFunction() {
+    let x = document.getElementById("ansTwo");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  /********Quest3**********/
+function myQuest3() {
+    let x = document.getElementById("ansThree");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+   /********Quest4*********/
+function myQuest4() {
+    let x = document.getElementById("ansFour");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
